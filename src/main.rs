38e65c1bbf4c281063e65690c9385a1132d6dd61
akononov <artem.kononov@zalando.de>
2022-04-04 18:39:31 +0200

    refactored

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,21 @@
-use std::collections::HashMap;
-use std::{fs, io};
-use std::fs::File;
-use std::io::{BufRead, BufReader};
-use std::path::PathBuf;
 use rand::Rng;
 
+fn main() {
+	let mut rng = rand::thread_rng();
+	let mood = rng.gen_range(0..5);
 
-fn main() -> io::Result<()> {
+	let contents = match mood {
+		0 => include_str!("../txt/go-for-it.txt"),
+		1 => include_str!("../txt/no-way.txt"),
+		2 => include_str!("../txt/too-late.txt"),
+		3 => include_str!("../txt/unhelpful.txt"),
+		4 => include_str!("../txt/you-sure.txt"),
+		_ => panic!("Bad value generated")
+	};
 
-    let files: HashMap<usize, PathBuf> = fs::read_dir("src/txt")?
-        .enumerate()
-        .map(|tpl| (
-            tpl.0,
-            tpl.1.expect("Failed to traverse file").path()
-        ))
-        .collect();
-
-    let mut rng = rand::thread_rng();
-    let rnd = rng.gen_range(0..files.len()) as usize;
-
-    let path = files.get(&rnd)
-        .expect(format!("No file at number {}", rnd).as_str());
-    let file = File::open(path.as_os_str())?;
-    let reader = BufReader::new(file);
-
-    let lns: Vec<String> = reader.lines()
-        .into_iter()
-        .map(|ln| ln.expect("Failed to read line"))
-        .collect();
-    let toln = rng.gen_range(0..lns.len());
-
-    let ln = lns.get(toln)
-        .expect(format!("Illegal line {}", toln).as_str());
-    println!("{}", ln);
-
-    Ok(())
+	let lines: Vec<&str> = contents
+		.split('\n')
+		.collect();
+	let line = rng.gen_range(0..lines.len());
+	println!("{}", lines.get(line).unwrap());
 }
-
